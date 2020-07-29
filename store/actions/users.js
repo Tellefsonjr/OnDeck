3bd0f277c4e0b30ef9adf822235cd7e0c85aba26
Tim Ellefson <timmey.ellefson@gmail.com>
2020-07-28 22:50:53 -0700

    some linterfixes not all

--- store/actions/users.js
+++ store/actions/users.js
@@ -2,6 +2,9 @@ export const ADD_JOB = 'ADD_JOB';
 export const UPDATE_JOB = 'UPDATE_JOB';
 export const DELETE_JOB = 'DELETE_JOB';
 
+export const SET_JOB_FILTERS = 'SET_JOB_FILTERS';
+
+
 export const ADD_JOB = (payload) => ({
     type: ADD_JOB,
     payload
@@ -16,3 +19,8 @@ export const DELETE_JOB = (payload) => ({
     type: DELETE_JOB,
     payload
 });
+
+export const SET_JOB_FILTERS = (payload) => ({
+    type: SET_JOB_FILTERS,
+    payload
+})
